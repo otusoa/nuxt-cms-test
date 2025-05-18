f30b56401dbf8c5f70edb0a1e6769d445f8a055f
Eita <kusaiyuka@gmail.com>
2025-05-19 06:58:07 +0900

    コンポーネント Hr.vue の構造を修正し、不要な要素を削除

--- app/components/global/Hr.vue
+++ app/components/global/Hr.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="hr">
-        <div class="hr-inner"></div>
+    <div>
+        <div class="hr"></div>
     </div>
 </template>
 
@@ -8,7 +8,7 @@
 </script>
 
 <style scoped>
-.hr-inner {
+.hr {
     border: 1px solid var(--subColor);
     height: 2px;
     background-color: var(--subColor);
