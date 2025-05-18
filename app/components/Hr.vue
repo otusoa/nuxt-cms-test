fa80fe31a5f345a2a22300ce9e4b852158ad29b1
Eita <kusaiyuka@gmail.com>
2025-05-18 22:21:02 +0900

    新規作成: Alert.vue, Hr.vue, Section.vue コンポーネントを追加

--- app/components/Hr.vue
+++ app/components/Hr.vue
@@ -1,16 +1,10 @@
 <template>
-    <div v-if="displayToggle">
+    <div>
         <div class="hr"></div>
     </div>
 </template>
 
 <script setup lang="ts">
-const { displayToggle } = defineProps({
-    displayToggle: {
-        type: Boolean,
-        default: false
-    }
-})
 </script>
 
 <style scoped>
